f3fa6cdeafbd6ac54dee746d2b4d762f751d0dbe
Robert Fisher <fisherpapa@gmail.com>
2011-03-30 12:52:24 -0700

    base keno classes and tests

---
+++ Keno.rb
@@ -0,0 +1,63 @@
+require 'date'
+require 'set'
+
+class Race
+  @@next_race_number = 1
+  attr_accessor :winners, :start, :number
+  def initialize (number = 1)
+    @winners = (1..80).to_a.shuffle![1..20]
+    @start = DateTime.now
+    @number = number
+  end
+end
+
+class Ticket
+  attr_accessor :choices, :races, :purchase_time
+  @@payouts = {
+    1 => {1 => 3},
+    2 => {1 => 1, 2 => 12},
+    3 => {2 => 1, 3 => 44},
+    4 => {2 => 1, 3 => 4, 4 => 120},
+    5 => {3 => 2, 4 => 14, 5 => 640},
+    6 => {3 => 1, 4 => 5, 5 => 80, 6 => 1800},
+    7 => {3 => 1, 4 => 3, 5 => 12, 6 => 125, 7 => 5000},
+    8 => {4 => 2, 5 => 7, 6 => 60, 7 => 675, 8 => 25000},
+    9 => {4 => 1, 5 => 5, 6 => 20, 7 => 210, 8 => 2500, 9 => 100000},
+    10 => {4 => 1, 5 => 2, 6 => 6, 7 => 50, 8 => 580, 9 => 10000, 10 => 500000}
+  }
+    
+  def initialize(choices, races)
+    @choices = choices
+    @races = races
+    @purchase_time = DateTime.now
+  end
+  
+  def get_payout(race)
+    raise "Invalid ticket!" if @purchase_time > race.start
+    correct = race.winners.to_set & @choices.to_set
+    @@payouts[@choices.count][correct.size]
+  end
+end
+
+class Keno
+  attr_accessor :races
+
+  def initialize
+    @races = {}
+    @current_index = 0
+  end
+  
+  def start_race
+    races[@current_index] =  Race.new
+    @current_index += 1
+    race
+  end
+  
+  def check_ticket(ticket)
+    payout = 0
+    ticket.races.each do |r| 
+      payout += ticket.get_payout @races[r]
+    end
+    payout
+  end 
+end
---
+++ KenoSuite.rb
@@ -0,0 +1,92 @@
+require '~/keno/Keno.rb'
+require 'minitest/autorun'
+require 'set'
+
+class TestRace < MiniTest::Unit::TestCase
+  def setup
+    @race = Race.new
+  end
+  
+  def test_that_there_are_twenty_winners
+    assert_equal 20, @race.winners.count
+  end
+  
+  def test_that_there_are_no_duplicates
+    assert_equal @race.winners.count, @race.winners.to_set.size
+  end
+end
+
+class Race
+  attr_accessor :winners, :start
+end
+
+class Ticket
+  def payouts
+    @@payouts
+  end
+end
+
+class TestTicket < MiniTest::Unit::TestCase    
+  def setup
+    @all_numbers = (1..80).to_a.shuffle!
+    @ticket_numbers = @all_numbers.take(10)
+  end
+  
+  def get_spots
+    data = []
+    (1..10).each do |spot_count|
+      current = @ticket_numbers.take(spot_count)
+      (0..spot_count).each do |hit_count|
+        spots = @ticket_numbers.take(hit_count)
+        spots.concat @all_numbers[80 - (spot_count - hit_count) .. 80]
+        spot_data = {spot_count: spot_count, hit_count: hit_count, spots: spots}
+        yield spot_data
+      end
+    end
+  end
+  
+  def test_all_payouts
+    #puts "#{ticket_numbers}"
+    get_spots do |spot_data| 
+      ticket = Ticket.new @ticket_numbers.take(spot_data[:spot_count]), [0]
+      race = Race.new
+      race.winners = spot_data[:spots]
+      payout = ticket.get_payout(race)
+      #puts "Spots:#{spot_data[:spot_count]}  #{spot_data[:hit_count]} #{payout}"
+      spots = spot_data[:spot_count]
+      hits = spot_data[:hit_count]
+      assert_equal ticket.payouts[spots][hits], payout
+    end
+  end
+  
+  def test_invalid_ticket
+    race = Race.new
+    race.start = DateTime.now - 10
+    ticket = Ticket.new [8,9,0], [0]
+    assert_raises(RuntimeError) {ticket.get_payout(race)}
+  end
+    
+end
+
+class TestKeno < MiniTest::Unit::TestCase
+  def test_check_ticket
+    mock = MiniTest::Mock.new
+  
+    race1 = Race.new
+    race2 = Race.new
+    race3 = Race.new
+  
+    mock.expect :races, [0,1,2]
+    mock.expect :get_payout, 1, [race1]
+   
+    k = Keno.new
+    k.races = [race1, race2, race3]
+  
+    assert_equal 3, k.check_ticket(mock)
+    
+    mock.verify
+  end
+end
+
+    
+    
